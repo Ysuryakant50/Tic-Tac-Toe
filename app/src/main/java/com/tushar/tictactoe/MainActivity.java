e78fdbd1ac42b9424ba7335bb165e67e950fbaee
Suryakant Yadav <ysuryakant2550@gmail.com>
2024-10-04 11:44:12 +0530

    Tic Tac Toe App complete

---
+++ app/src/main/java/com/tushar/tictactoe/MainActivity.java
@@ -0,0 +1,33 @@
+package com.tushar.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText, editText2;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        editText2 = findViewById(R.id.editText2);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectLayoutActivity.class);
+                intent.putExtra("player1", editText.getText().toString());
+                intent.putExtra("player2", editText2.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
